969ff78443d7ca542032abc025649f800debaa77
pdvladking <pdvladking@gmail.com>
2026-02-11 11:22:44 +0545

    Add: backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoutes from './routes/auth.js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Connect MONGODB
+mongoose
+  .connect('', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
